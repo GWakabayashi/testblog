f5bb895fb64935a8ff419c7c0b88545f627a9dc8
“Linear <raipons.wood@gmail.com>
2022-05-22 10:48:06 +0200

    added cancel button for blog comments

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -9,10 +9,30 @@ export default class extends Controller {
     event.stopPropagation();
     const formID = event.params["form"];
     const commentContentID = event.params["content"];
+    const editButtonId = event.params["edit"];
+
     const form = document.getElementById(formID);
+    const commentContent = document.getElementById(commentContentID);
+    const editButton = document.getElementById(editButtonId);
+
     form.classList.toggle("d-none");
     form.classList.toggle("mt-5");
-    const commentContent = document.getElementById(commentContentID);
     commentContent.classList.toggle("d-none");
+    this.toggleEditButton(editButton);
+  }
+
+  toggleEditButton(editButton) {
+    if (editButton.innerText === "Edit") {
+      editButton.innerText = "Cancel";
+      this.toggleEditButtonClass(editButton);
+    } else {
+      editButton.innerText = "Edit";
+      this.toggleEditButtonClass(editButton);
+    }
+  }
+
+  toggleEditButtonClass(editButton) {
+    editButton.classList.toggle("btn-secondary");
+    editButton.classList.toggle("btn-warning");
   }
 }
